1dee62e4d2e9dc21078082f34044eb5911629b3c
mmatczak <marek.matczak79@gmail.com>
2023-10-16 16:21:24 +0200

    Promise / MVVM

--- index.js
+++ index.js
@@ -1,19 +1,67 @@
-const firstNameElement = document.querySelector('#firstName');
-setTimeout(() => {
-    console.log('Now calling setTimeout handler...')
-    firstNameElement.value = 'James Bond';
-}, 1000);
-firstNameElement.setAttribute('value', 'Zdzisław');
-// console.log('Now setting prop...')
-firstNameElement.value = 'Czarek';
-console.log("Prop: ", firstNameElement.value);
-console.log("Attr: ", firstNameElement.getAttribute('value'));
+// Model, View, View Model
 
-firstNameElement.addEventListener('input', function (event) {
-    console.log(event.target.value);
-})
+// Model
+class User {
+    id;
 
+    constructor(firstName, lastName) {
+        this.id = User.idSeq++;
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+}
 
-// data goes down
-// events go up
+User.idSeq = 0;
+
+class UserService {
+    findById(userId) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(new User('John', 'Smith'));
+                // reject('Oh no...');
+            }, 2000);
+        })
+    }
+}
+
+// View Model
+class UserComponent {
+    constructor(userService) {
+        this.userService = userService;
+        document.querySelector('button')
+            .addEventListener('click', event => this.save());
+    }
+
+    get firstName() {
+        return document.getElementById('firstName').value;
+    }
+
+    set firstName(newValue) {
+        document.getElementById('firstName').value = newValue;
+    }
+
+    get lastName() {
+        return document.getElementById('firstName').value;
+    }
+
+    set lastName(newValue) {
+        document.getElementById('lastName').value = newValue;
+    }
+
+    loadUser(userId) {
+        this.userService.findById(userId).then(user => {
+            this.firstName = user.firstName;
+            this.lastName = user.lastName;
+        }, error => console.error('Uops...', error));
+    }
+
+    save() {
+        const updatedUser = new User(this.firstName, this.lastName);
+        // this.userService.save(updatedUser);
+        console.log('Saving: ', updatedUser);
+    }
+}
+
+const userComponent = new UserComponent(new UserService());
+userComponent.loadUser(1234);
 
